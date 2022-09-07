9258c1fc0eddff3a194857b2745b8daabd70642f
Alexx <al_nesteov@inbox.ru>
2022-09-07 23:39:21 +0300

    Homework for seminar3 task Store add function

--- src/Communication.java
+++ src/Communication.java
@@ -7,6 +7,7 @@ public interface Communication {                    // интерфейс нав
         return item.getCost();                          // стоимость товара
     }
 
+
     default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer) {
 
         for (int i = 0; i < wishListOfConsumer.size(); i++) {
@@ -19,23 +20,73 @@ public interface Communication {                    // интерфейс нав
             }
             if (count == 0) {
                 System.out.println("у нас нет  " + wishListOfConsumer.get(i));//  вывод товара которого нет у продавца
+                int count1 = 0;
+
+                String str2 = wishListOfConsumer.get(i).toString();
+                String[] str3 = wishListOfConsumer.get(i).toString().split(" ");
+
+                for (int j = 0; j < seller.getItems().size(); j++) {
+                    String str1 = seller.getItems().get(j).toString();
+
+
+                    if (str1.contains(str3[0])) {
+                        if (count1 == 0) System.out.println("могу предложить взамен: ");
+                        System.out.println(seller.getItems().get(j) + " про цене: " + costOfItem(seller.getItems().get(j)));
+                        count1++;
+                    }
+                }
+
             }
+
         }
     }
+
+
+//
+//    default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer) {
+//
+//        for (int i = 0; i < wishListOfConsumer.size(); i++) {
+//            int count = 0;
+//
+//            for (int j = 0; j < seller.getItems().size(); j++) {
+//                if (seller.getItems().get(j).toString() == (wishListOfConsumer.get(i)).toString()) {
+//                    count++;
+//                }
+//            }
+//            if (count == 0) {
+//                System.out.println("у нас нет  " + wishListOfConsumer.get(i));//  вывод товара которого нет у продавца
+//                int count1 = 0;
+//
+//                String str2 = wishListOfConsumer.get(i).toString();
+//
+//                for (int j = 0; j < seller.getItems().size(); j++) {
+//                    String str1 = seller.getItems().get(j).toString();
+//
+//                    if (str1.contains(str2) ){
+//                        if (count1 == 0) System.out.println("могу предложить взамен: ");
+//                        System.out.println(seller.getItems().get(j) + " про цене: " + costOfItem(seller.getItems().get(j)));
+//                        count1++;
+//                    }
+//                }
+//
+//            }
+//
+//        }
+//    }
 
 
 //    default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer) {           // метод отсутствия позиций на складе товара, указанного в списке покупателя
 //
 //        for (Item item : wishListOfConsumer) {                                             // список желаний покупателя
 //
-//            if (!seller.getItems().contains(item.getNameOfItem())) {                                      // если у продавца на складе нет товара
+//            if (!seller.getItems().toString().equals(item.getNameOfItem().toString())) {                                      // если у продавца на складе нет товара
 //                System.out.println("у нас нет  " + item.getNameOfItem());//  вывод товара которого нет у продавца
 //
 //            }
 //        }
-////        System.out.println("товар на складе у продавца: " + seller.getItems());
-////        System.out.println("список покупок покупателя - " + wishListOfConsumer);
-
+//        System.out.println("товар на складе у продавца: " + seller.getItems());
+//        System.out.println("список покупок покупателя - " + wishListOfConsumer);
+//
 //    }
 
 
@@ -51,6 +102,9 @@ public interface Communication {                    // интерфейс нав
     default void areYouPoor(Consumer consumer, Seller seller) {                                            // метод нищий
         if (totalCost(consumer.getBusket()) > consumer.getMoney()) {                                  // если общая стоимость покупки больше, чем денег у покупателя
             System.out.println("Нет хватает  денег на покупку!");
+            double payExtra = totalCost(consumer.getBusket()) - consumer.getMoney();
+            System.out.println("Вам надо доплатить : " + payExtra);
+
             consumer.walkInStorage();
         } else {
             seller.walkInStorage();
